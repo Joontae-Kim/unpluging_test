025993e1f91b766dfeb3014940e86e897b63c0b4
bexellenting@gmail.com <bexellenting@gmail.com>
2017-03-09 19:21:02 +0900

    customize firebase3.0 facebook login

--- apps/static/js/js_git/firebase3.0_login.js
+++ apps/static/js/js_git/firebase3.0_login.js
@@ -1,19 +1,27 @@
     /**
      * Function called when clicking the Login/Logout button.
      */
-
     // [START buttoncallback]
     function toggleSignIn() {
       if (!firebase.auth().currentUser) {
-        var provider = new firebase.auth.FacebookAuthProvider(); // [START createprovider]
-        provider.addScope('user_likes'); // [START addscopes]
-        firebase.auth().signInWithRedirect(provider); // [START signin]
+        // [START createprovider]
+        var provider = new firebase.auth.FacebookAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('user_likes');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithRedirect(provider);
+        // [END signin]
       } else {
+        // [START signout]
         firebase.auth().signOut();
+        // [END signout]
       }
-      document.getElementById('quickstart-sign-in').disabled = true; // [START_EXCLUDE]
+      // [START_EXCLUDE]
+      document.getElementById('quickstart-sign-in').disabled = true;
+      // [END_EXCLUDE]
     }
-
     // [END buttoncallback]
     /**
      * initApp handles setting up UI event listeners and registering Firebase auth listeners:
@@ -22,53 +30,71 @@
      *  - firebase.auth().getRedirectResult(): This promise completes when the user gets back from
      *    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
      */
-
     function initApp() {
       // Result from Redirect auth flow.
-      firebase.auth().getRedirectResult().then(function(result) { // [START getidptoken]
+      // [START getidptoken]
+      firebase.auth().getRedirectResult().then(function(result) {
         if (result.credential) {
           // This gives you a Facebook Access Token. You can use it to access the Facebook API.
           var token = result.credential.accessToken;
-          document.getElementById('quickstart-oauthtoken').text = token; // [START_EXCLUDE]
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-oauthtoken').textContent = token;
         } else {
-          document.getElementById('quickstart-oauthtoken').text = 'null';
+          document.getElementById('quickstart-oauthtoken').textContent = 'null';
+          // [END_EXCLUDE]
         }
-        var user = result.user; // The signed-in user info.
+        // The signed-in user info.
+        var user = result.user;
       }).catch(function(error) {
-        var errorCode = error.code; // Handle Errors here.
+        // Handle Errors here.
+        var errorCode = error.code;
         var errorMessage = error.message;
-        var email = error.email; // The email of the user's account used.
-        var credential = error.credential; // The firebase.auth.AuthCredential type that was used.        
-        if (errorCode === 'auth/account-exists-with-different-credential') { // [START_EXCLUDE]
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/account-exists-with-different-credential') {
           alert('You have already signed up with a different auth provider for that email.');
           // If you are using multiple auth providers on your app you should handle linking
           // the user's accounts here.
         } else {
           console.error(error);
         }
+        // [END_EXCLUDE]
       });
-      
+      // [END getidptoken]
       // Listening for auth state changes.
-      firebase.auth().onAuthStateChanged(function(user) { // [START authstatelistener]
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          var displayName = user.displayName; // User is signed in.
+          // User is signed in.
+          var displayName = user.displayName;
           var email = user.email;
           var emailVerified = user.emailVerified;
           var photoURL = user.photoURL;
           var isAnonymous = user.isAnonymous;
           var uid = user.uid;
           var providerData = user.providerData;
-          document.getElementById('quickstart-sign-in-status').text = 'Signed in';
-          document.getElementById('quickstart-sign-in').text = 'Log out';
-          document.getElementById('quickstart-account-details').text = JSON.stringify(user, null, '  ');
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+          document.getElementById('quickstart-sign-in').textContent = 'Log out';
+          document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+          // [END_EXCLUDE]
         } else {
-          document.getElementById('quickstart-sign-in-status').textContent = 'Signed out'; // User is signed out.
+          // User is signed out.
+          // [START_EXCLUDE]
+          document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
           document.getElementById('quickstart-sign-in').textContent = 'Log in with Facebook';
           document.getElementById('quickstart-account-details').textContent = 'null';
           document.getElementById('quickstart-oauthtoken').textContent = 'null';
+          // [END_EXCLUDE]
         }
+        // [START_EXCLUDE]
         document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
       });
+      // [END authstatelistener]
       document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
     }
     window.onload = function() {
